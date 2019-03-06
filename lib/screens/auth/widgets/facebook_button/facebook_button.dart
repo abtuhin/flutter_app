473c6c5d8e2c99cbed3611c49bc3564aa7350372
tuhin <mosabbir@shopf.co>
2019-03-05 16:58:40 -0800

    first commit

---
+++ lib/screens/auth/widgets/facebook_button/facebook_button.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/widgets/app_button/index.dart';
+
+class FacebookButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new AppButton(
+      buttonName: "facebook",
+      onPressed: null,
+      buttonTextStyle: null
+    );
+  }
+}
---
+++ lib/screens/auth/widgets/google_button/google_button.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/widgets/app_button/index.dart';
+
+class GoogleButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new AppButton(
+      buttonName: "Google",
+      onPressed: null,
+      buttonTextStyle: null
+    );
+  }
+}
